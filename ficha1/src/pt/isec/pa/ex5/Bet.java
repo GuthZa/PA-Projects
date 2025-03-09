590888bcfaa4d0de4d642367035bc2595314942e
Wisura <joao9.claro99@gmail.com>
2025-03-09 12:14:06 +0000

    start ex5

---
+++ ficha1/src/pt/isec/pa/ex5/Bet.java
@@ -0,0 +1,12 @@
+package pt.isec.pa.ex5;
+
+public class Bet {
+    private int luckyNumber;
+    private int[] betNumbers;
+    public Bet() {
+    }
+
+    public void start() {
+
+    }
+}
---
+++ ficha1/src/pt/isec/pa/ex5/Main.java
@@ -0,0 +1,8 @@
+package pt.isec.pa.ex5;
+
+public class Main {
+    public static void main(String[] args) {
+        Bet bet = new Bet();
+        bet.start();
+    }
+}
