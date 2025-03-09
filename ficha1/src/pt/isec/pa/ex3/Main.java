e8b4566d5d75619f3cb0b4c42fd1be60aa588132
Wisura <joao9.claro99@gmail.com>
2025-03-09 11:01:40 +0000

    Start of ex3

---
+++ ficha1/src/pt/isec/pa/ex3/Main.java
@@ -0,0 +1,8 @@
+package pt.isec.pa.ex3;
+
+public class Main {
+    public static void main(String[] args) {
+        StaticArrays staticArrays = new StaticArrays();
+        staticArrays.start();
+    }
+}
---
+++ ficha1/src/pt/isec/pa/ex3/StaticArrays.java
@@ -0,0 +1,10 @@
+package pt.isec.pa.ex3;
+
+public class StaticArrays {
+    public StaticArrays() {
+    }
+
+    public void start() {
+
+    }
+}
