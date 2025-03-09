bc9a963ce8910ec0f24509e99a61b6ce9e47cd0c
Wisura <joao9.claro99@gmail.com>
2025-03-09 10:21:08 +0000

    Extra flair in ex1 and start of ex2

--- ficha1/src/pt/isec/pa/ex1/GuessTheNumber.java
+++ ficha1/src/pt/isec/pa/ex1/GuessTheNumber.java
@@ -4,32 +4,42 @@ import java.util.Scanner;
 
 public class GuessTheNumber {
 
+    private int numberToGuess;
+
     public GuessTheNumber() {}
 
     public void start() {
         Scanner sc = new Scanner(System.in);
         int guess = 0;
+        numberToGuess = generateNewNumber();
+        do {System.out.println("Guess the number between 0 and 100! (-1 to leave)");
+
+            if (sc.hasNextInt() &&
+                    (guess = sc.nextInt()) == -1)
+                return;
+
+            checkNumber(guess);
+
+        } while (true);
+    }
+
+    private int generateNewNumber() {
         //0.0 - 0.99
         //0.0 - 99.99
         //Restriction: User Math.Random
-        int numberToGuess = (int) (Math.random() * 101);
-        do {
-            System.out.println("Guess the number! (0 to 100)");
-            if (sc.hasNextInt())
-                guess = sc.nextInt();
+        return (int) (Math.random() * 101);
+    }
 
-            if (guess > 100 || guess < 0)
-                System.out.println("Please write a valid number");
+    private void checkNumber(int numberToCheck) {
+        if (numberToCheck > 100 || numberToCheck < 0)
+            System.out.println("Please write a valid number");
 
-            if (numberToGuess > guess) {
-                System.out.println("Close, but it's higher");
-            } else if (numberToGuess < guess) {
-                System.out.println("Close, but it's lower");
-            } else {
-                System.out.println("You found the number! Congratulations!");
-                return;
-            }
-
-        } while (true);
+        if (numberToGuess > numberToCheck) {
+            System.out.println("Close, but it's higher");
+        } else if (numberToGuess < numberToCheck) {
+            System.out.println("Close, but it's lower");
+        } else {
+            System.out.println("You found the number! Congratulations!");
+        }
     }
 }
---
+++ ficha1/src/pt/isec/pa/ex2/Main.java
@@ -0,0 +1,7 @@
+package pt.isec.pa.ex2;
+
+public class Main {
+    private static void main(String[] args) {
+        
+    }
+}
